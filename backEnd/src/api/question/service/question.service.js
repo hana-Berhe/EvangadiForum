c67e63014d0e0f9eb82f98ea8d0f87b421802073
Abela-code <abelambachew11@gmail.com>
2026-09-12 22:50:25 +0300

    feat: insert question into database

--- backEnd/src/api/question/service/question.service.js
+++ backEnd/src/api/question/service/question.service.js
@@ -6,3 +6,24 @@ const generateQuestionHash = () => crypto.randomBytes(8).toString("hex");
 
 const createQuestionWithVectorService = async (payload) => {
   const { userId, title, content } = payload;
+  const insertQuestionSql =
+  "INSERT INTO questions (question_hash, user_id, title, content) VALUES (?, ?, ?, ?)";
+
+const questionHash = generateQuestionHash();
+
+let questionResult;
+
+try {
+  questionResult = await safeExecute(insertQuestionSql, [
+    questionHash,
+    userId,
+    title,
+    content,
+  ]);
+} catch (error) {
+  if (error?.code === "ER_NO_REFERENCED_ROW_2") {
+    throw new BadRequestError("User does not exist.");
+  }
+
+  throw error;
+}

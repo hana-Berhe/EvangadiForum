55e11c6d827ed9a64a34af5959df2083166446e7
Abela-code <abelambachew11@gmail.com>
2026-09-12 22:50:02 +0300

    feat: initialize question creation service

--- backEnd/src/api/question/service/question.service.js
+++ backEnd/src/api/question/service/question.service.js
@@ -1,5 +1,8 @@
 import crypto from "crypto";
 import { safeExecute } from "../../../../schema/db.config.js";
 
-
 const generateQuestionHash = () => crypto.randomBytes(8).toString("hex");
+
+
+const createQuestionWithVectorService = async (payload) => {
+  const { userId, title, content } = payload;

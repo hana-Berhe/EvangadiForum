bc528a31961f75004f2af97b5ece4bda3d35ec0d
haymi86 <haymanotyetwale86@gmail.com>
2026-09-12 20:42:25 +0300

    feat: add answer validation and controller

---
+++ backEnd/src/api/answer/validations/answer.validation.js
@@ -0,0 +1,24 @@
+import { body, param, query } from "express-validator";
+import { validationErrorHandler } from "../../../middleware/validation-handler.js";
+
+const createAnswerValidation = [
+  body("questionId")
+    .notEmpty()
+    .withMessage("Question id is required")
+    .isInt({ min: 1 })
+    .withMessage("Question id must be a positive integer")
+    .toInt(),
+  body("content")
+    .notEmpty()
+    .withMessage("Answer content is required")
+    .isString()
+    .withMessage("Answer content must be a string")
+    .isLength({ min: 20 })
+    .withMessage("Answer content must be at least 20 characters")
+    .trim(),
+  validationErrorHandler,
+];
+
+export {
+  createAnswerValidation
+};

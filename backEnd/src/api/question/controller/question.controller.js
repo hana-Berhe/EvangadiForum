8d96e9c8a024f084098bd2b6e28db40528742f02
Abela-code <abelambachew11@gmail.com>
2026-09-12 22:37:45 +0300

    feat: implement question creation controller

--- backEnd/src/api/question/controller/question.controller.js
+++ backEnd/src/api/question/controller/question.controller.js
@@ -1,1 +1,21 @@
 import { StatusCodes } from "http-status-codes";
+
+
+const createQuestionController = async (req, res, next) => {
+  try {
+    const { title, content } = req.body;
+    const result = await createQuestionWithVectorService({
+      userId: req.user.id, // author id (authenticated user)
+      title,
+      content,
+    });
+
+    res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "Question posted successfully.",
+      data: result.question,
+    });
+  } catch (error) {
+    next(error);
+  }
+};

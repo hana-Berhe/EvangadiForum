da1334213e0039c9a39299187a50d924b5f41881
Abela-code <abelambachew11@gmail.com>
2026-09-13 00:22:17 +0300

    feat: add question route dependencies

---
+++ backEnd/src/api/question/routes/question.routes.js
@@ -0,0 +1,7 @@
+import {
+  createQuestionController,
+} from "../controller/question.controller.js";
+import {
+  createQuestionValidation,
+} from "../validations/question.validation.js";
+import { authenticateUser } from "../../../middleware/authentication.js";

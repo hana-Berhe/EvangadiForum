9becd97678275fd36e7af5b9994e6f732f6ef29c
Abela-code <abelambachew11@gmail.com>
2026-09-14 14:13:14 +0300

    feat: add main application layout

--- frontEnd/src/components/Layout/Layout.jsx
+++ frontEnd/src/components/Layout/Layout.jsx
@@ -3,3 +3,21 @@ import Sidebar from "../Sidebar/Sidebar";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import styles from "./Layout.module.css";
+
+export default function Layout() {
+  return (
+    <div className={styles.appLayout}>
+      <Sidebar />
+
+      <div className={styles.appMain}>
+        <Navbar />
+
+        <main className={styles.pageContent}>
+          <Outlet />
+        </main>
+
+        <Footer />
+      </div>
+    </div>
+  );
+}

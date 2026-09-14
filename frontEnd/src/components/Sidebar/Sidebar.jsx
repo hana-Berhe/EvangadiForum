1c5275a37e7a9112805af53d330ff7e45a997767
Abela-code <abelambachew11@gmail.com>
2026-09-14 13:32:12 +0300

    feat: add sidebar dependencies and imports

---
+++ frontEnd/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import { NavLink, useNavigate } from "react-router-dom";
+
+import { BookOpen, Home, LogOut, MessageSquare, Plus } from "lucide-react";
+
+import { useAuth } from "../../context/AuthContext";
+
+import styles from "./Sidebar.module.css";
+
+import ui from "../../styles/pageStates.module.css";
